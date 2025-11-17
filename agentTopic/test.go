09f64e7d6a21440da59778c1e9ea2fb0b1ab104a
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-17 09:58:17 +0100

    Feat: Add net package import for enhanced system metrics collection

--- agentTopic/test.go
+++ agentTopic/test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/shirou/gopsutil/v3/process"
+	"github.com/shirou/gopsutil/v3/net"
 )
 
 var writer *kafka.Writer
