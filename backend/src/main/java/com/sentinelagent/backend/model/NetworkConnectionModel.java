8039ce590c54275fc5f96447ad75262769d8b778
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-21 17:28:51 +0100

    Feat: Add NetworkConnectionModel to represent network connection details

---
+++ backend/src/main/java/com/sentinelagent/backend/model/NetworkConnectionModel.java
@@ -0,0 +1,17 @@
+package com.sentinelagent.backend.model;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NetworkConnectionModel {
+    private int pid;
+    private String localAddress;
+    private int localPort;
+    private String remoteAddress;
+    private int remotePort;
+    private String status;
+}
