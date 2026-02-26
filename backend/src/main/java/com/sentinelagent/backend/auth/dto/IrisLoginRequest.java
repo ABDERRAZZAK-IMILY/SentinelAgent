60ba61b20c998daf22a084e8cdbb3e72af6dd19c
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-02-26 10:25:44 +0000

    Add InvalidAgentCredentialsException for handling authentication errors

---
+++ backend/src/main/java/com/sentinelagent/backend/auth/dto/IrisLoginRequest.java
@@ -0,0 +1,16 @@
+package com.sentinelagent.backend.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class IrisLoginRequest {
+    private String username;
+    private String apiKey;
+}
