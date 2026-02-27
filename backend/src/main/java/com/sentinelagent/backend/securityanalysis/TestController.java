95b8f61f0671ca2c8c77d9169f776452ceebe916
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-02-27 11:45:07 +0000

    Refactor simulateAttack method in TestController to return AnalysisResult and enhance metric report details

--- backend/src/main/java/com/sentinelagent/backend/securityanalysis/TestController.java
+++ backend/src/main/java/com/sentinelagent/backend/securityanalysis/TestController.java
@@ -1,9 +1,10 @@
 package com.sentinelagent.backend.securityanalysis;
 
-import com.sentinelagent.backend.securityanalysis.AnalyzeSecurityUseCase;
+import com.sentinelagent.backend.securityanalysis.domain.AnalysisResult;
 import com.sentinelagent.backend.telemetry.MetricReport;
 import com.sentinelagent.backend.telemetry.Process;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +19,23 @@ public class TestController {
     private final AnalyzeSecurityUseCase analyzeSecurityUseCase;
 
     @PostMapping("/simulate-attack")
-    public String simulateAttack() {
+    public ResponseEntity<AnalysisResult> simulateAttack() {
         MetricReport fakeReport = MetricReport.builder()
-                .cpuUsage(20.5)
-                .ramUsedPercent(20.0)
+                .agentId("test-agent-001")
+                .hostname("Testing-PC")
+                .cpuUsage(95.5)
+                .ramUsedPercent(80.0)
+                .bytesSentSec(5000000L)
                 .processes(List.of(
                         Process.builder()
-                                .name("facebook.exe")
+                                .name("suspicious_miner.exe")
                                 .pid(666)
+                                .cpuUsage(70.5)
                                 .build()))
                 .build();
 
-        return analyzeSecurityUseCase.execute(fakeReport);
+        AnalysisResult result = analyzeSecurityUseCase.execute(fakeReport);
+
+        return ResponseEntity.ok(result);
     }
 }
