a2dadb02f20835021a30f46361de0a4495f5651c
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-03-16 15:05:07 +0000

    Add overview selectors for accessing dashboard state in NgRx

---
+++ frontend/src/app/features/overview/store/overview.selectors.ts
@@ -0,0 +1,10 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { OverviewState } from './overview.state';
+
+export const selectOverviewState = createFeatureSelector<OverviewState>('overview');
+
+export const selectOverviewLoading = createSelector(selectOverviewState, (state) => state.loading);
+export const selectOverviewError = createSelector(selectOverviewState, (state) => state.error);
+export const selectOverviewUnitSummary = createSelector(selectOverviewState, (state) => state.unitSummary);
+export const selectOverviewPerformance = createSelector(selectOverviewState, (state) => state.performance);
+export const selectOverviewLogs = createSelector(selectOverviewState, (state) => state.logs);
