e318a12a80e6dc2810fb30ca1fe12a0a32d3cd53
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-03-17 11:33:58 +0000

    Rename IrisLoginRequest to SendCommandRequest and update package structure

--- backend/src/main/java/com/sentinelagent/backend/agent/dto/SendCommandRequest.java
+++ backend/src/main/java/com/sentinelagent/backend/agent/dto/SendCommandRequest.java
@@ -1,16 +1,16 @@
-package com.sentinelagent.backend.auth.dto;
+package com.sentinelagent.backend.agent.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class IrisLoginRequest {
-    private String username;
-    private String apiKey;
+public class SendCommandRequest {
+    private String command;
+    private String parameters;
 }
+
