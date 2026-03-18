118213eee0bcd8ef506ad3e9b8f277b9eaffafa7
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-03-18 12:06:09 +0000

    Add route properties to quickLinks in overview-page component

--- frontend/src/app/features/overview/components/overview-control-panel.component.ts
+++ frontend/src/app/features/overview/components/overview-control-panel.component.ts
@@ -1,9 +1,11 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { QuickLink } from '../../../core/models/view.model';
 
 @Component({
   selector: 'app-overview-control-panel',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './overview-control-panel.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
